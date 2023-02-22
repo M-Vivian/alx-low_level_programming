7375a1857a3d0cb3aca029d9d353833a79819f7d
M-Vivian <veeeeeeee99@gmail.com>
2023-02-21 21:09:30 -0800

    script that runs a C file through the preprocessor and save the result into another file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* C program to show use of puts */
+/* Value should end with 0 */
+#include <stdio.h>
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle \n");
+  return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/* C programming use of printf */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
