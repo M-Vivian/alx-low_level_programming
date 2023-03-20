99bc7888b879b017855b182c6836817b7a2e8182
M-Vivian <veeeeeeee99@gmail.com>
2023-03-20 05:15:56 -0700

    function-like macro ABS(x)

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,4 @@
-#ifndef ABS
-#define ABS(x) (((x) < (0)) ? ((x) * (-1)) : (x))
-#endif /*ABS*/
+#ifndef THREE_FUNCTION_LIKE_MACRO_H
+#define THREE_FUNCTION_LIKE_MACRO_H
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif /* THREE_FUNCTION_LIKE_MACRO_H */
